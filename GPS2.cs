4bfe9d1838f5f63f534d968d9a1189c5a6193627
hmanning32 <132292281+hmanning32@users.noreply.github.com>
2023-05-01 13:17:37 -0500

    Create GPS2.cs

---
+++ GPS2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Waste3
+{
+    
+    // intializes values from sql table 
+    internal class GPS2
+    {
+        public int ID { get; set; }
+
+        public string Coordinates { get; set; }
+
+        public DateTime Date_ { get; set; }
+
+
+    }
+}
